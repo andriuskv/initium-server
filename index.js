c587a4c643aa1cdfa894a24d5bbea811b65597c6
Andrius Kvedaras <ikandrius@gmail.com>
2019-01-06 15:27:50 +0200

    Add OpenWeatherMap endpoint

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors({
 
 app.use("/feed", require("./feed.js"));
 app.use("/twitter", require("./twitter.js"));
+app.use("/owm", require("./owm.js"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
